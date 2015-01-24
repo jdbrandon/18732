b7f5f988315b11314243602e98e999a9357c9298
John Gilmore Bush <john.gilmore.bush@gmail.com>
2015-01-23 20:38:41 -0500

    Working on getting shellcode to work.

---
+++ shellcode1.asm
@@ -0,0 +1,59 @@
+Section   .text
+     global _start
+
+;_start:
+;     jmp short GotoCall
+;shellcode:
+;     pop esi
+;     xor eax, eax
+;     mov byte [esi + 7], al
+;     mov byte [esi + 10], al
+;     mov byte [esi + 23], al
+;     mov byte [esi + 24], al
+;     lea ebx, [esi]
+;     mov long [esi + 25], ebx
+;     mov long [esi + 29], eax
+;     mov byte al, 0x0b
+;     mov ebx, esi
+;     lea ecx, [esi + 25]
+;     lea edx, [esi + 29]
+;     int 0x80
+;
+;GotoCall:
+;     Call shellcode
+;     db '/bin/shJ-cJ"echo Hello"JJAAAAKKKK'
+
+_start:
+     jmp short arg3
+shellcode:
+     pop esi
+     xor eax, eax
+     mov byte [esi + 7], al
+     lea ebx, [esi]
+     mov long [esi + 8], ebx
+     mov long [esi + 12], eax
+     mov byte al, 0x0b
+     mov ebx, esi
+     lea ecx, [esi + 8]
+     lea edx, [esi + 12]
+     int 0x80
+
+arg3:
+     xor eax, eax
+     sub long esp, 0x4
+     mov long [esp], eax
+arg2:
+     Call arg1
+     db 'echo HelloJ'
+arg1:
+     pop esi
+     push esi
+     mov byte [esi + 10], al
+     Call arg0
+     db '-cJ'
+arg0:
+     pop esi
+     push esi
+     mov byte [esi + 2], al
+     Call shellcode
+     db '/bin/shJAAAAKKKK'
